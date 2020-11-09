f27ea0f7654e636c91e1b017e270ae45721d1e58
bework <bework@qq.com>
2020-11-09 17:53:33 +0800

    init

---
+++ src/Config.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Becage\Weather;
+
+class Config
+{
+    const GAODE = 'https://restapi.amap.com/v3/weather/weatherInfo';
+}
---
+++ src/Exceptions/InvalidArgumentException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Becage\Weather\Exceptions;
+
+class InvalidArgumentException extends Exception
+{
+
+}
